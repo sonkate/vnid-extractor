49842294e8328640b5cf96e2c6564317805ca27c
sonkate <tranthanhson03082002@gmail.com>
2024-05-08 21:22:53 +0700

    update port

--- src/main.py
+++ src/main.py
@@ -36,4 +36,4 @@ async def predict_api(f: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
